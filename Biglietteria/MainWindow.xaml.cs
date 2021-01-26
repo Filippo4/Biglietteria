912cacff135427176a4dc549f0c8d5c611659e95
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-26 12:50:42 +0100

    aggiunto tasto aggiungi Cliente

--- Biglietteria/MainWindow.xaml.cs
+++ Biglietteria/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClasseCliente;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,48 @@ namespace Biglietteria
 
         private void btn_aggiungiCliente_Click(object sender, RoutedEventArgs e)
         {
+            Cliente cli = new Cliente(txt_nome.Text, txt_cognome.Text);
+            try
+            {
+                string nome = null;
+                string cognome = null;
 
+               
+
+                if (txt_nome.Text != null)
+                    nome = txt_nome.Text;
+                else
+                    MessageBox.Show("il nome non può essere vuota", "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                if (txt_cognome.Text != null)
+                    cognome = txt_cognome.Text;
+                else
+                    MessageBox.Show("il nome non può essere vuota", "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+                cli.SetSesso(rbtn_maschio.IsChecked == true);
+                cli.SetNumero(txt_cellulare.Text);
+
+                if (rbtn_maschio.IsChecked == true)
+                    cli.SetSesso(true);
+                else
+                    cli.SetSesso(false);
+
+                cli.SetNumero(txt_cellulare.Text);
+
+                cmb_cliente.Items.Add(cli.Stampa());
+                cmb_cliente.Items.Add(cli.Stampa());
+
+                txt_cellulare.Clear();
+                txt_cognome.Clear();
+                txt_nome.Clear();
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+             
         }
     }
 }
---
+++ Cliente/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClasseCliente
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+        public string cognome { get; set;}
+        private string sesso;
+        private string numero;
+        public Cliente(string n ,string c)
+        {
+            nome = n;
+            cognome = c;            
+        }
+        public string GetSesso()
+        {
+            return sesso;
+        }
+
+        public void SetSesso(bool s)
+        {
+            if (s == true)
+                sesso = "M";
+            else
+                sesso = "F";         
+        }
+        public string GetNumero()
+        {
+            return numero;
+        }
+        public void SetNumero(string n)
+        {
+            if (n.Length == 10)
+            {
+                try
+                {
+                    int cellulare = int.Parse(n);
+                }
+                catch(Exception)
+                {
+                    throw new Exception("Inserisci soltanto numeri!");
+                }
+            }
+            else
+               throw new Exception("il numero non è valido!");
+        }
+        public string Stampa()
+        {
+            return $"{nome} {cognome}";
+        }
+    }
+}
